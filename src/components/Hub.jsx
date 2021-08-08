4d6e686c3122a331ce9de8f64f73d66594bd5ea5
Andrew <wing_andrew@hotmail.com>
2021-08-08 00:15:24 -0400

    Preparing hub

--- src/components/Hub.jsx
+++ src/components/Hub.jsx
@@ -1,2 +1,23 @@
 // WHEN I load the portfolio the first time
 // THEN the About Me title and section are selected by default
+
+import React from 'react'
+import './styles/hub.scss'
+import Routes from '../Routes'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+export default function Hub() {
+    return (
+       <BrowserRouter>
+         <Route render = {(props) => (
+            <div className='hub'>
+                <div className="hub__content" >              
+                    <div className="hub__content_main">
+                        <Routes/>
+                    </div>
+                </div>
+            </div>
+          )}/>
+        </BrowserRouter>
+    );
+};
