ebf33ad005c8647e313d553433f80705df27bd19
Andrew <wing_andrew@hotmail.com>
2021-08-08 09:19:29 -0400

    Implementing about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,15 @@
 // WHEN I am presented with the About Me section
 // THEN I see a recent photo or avatar of the developer and a short bio about them
 
-import React from 'react'
+import React from 'react';
+import pic from './../images/andrew.png';
 
 function About() {
     return (
         <content>
-            <p>About</p>
+            <h1>About Me</h1>
+            <img src={pic} alt="Recent photo of Andrew Wing" />
+            <p>My name is Andrew Wing and I am a recent Full-Stack web development bootcamp graduate living in Ottawa, Ontario, Canada</p>
         </content>
       
     );
