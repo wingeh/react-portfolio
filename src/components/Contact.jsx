2e92396b4e79a36e280668adb37f407b8f844283
Andrew <wing_andrew@hotmail.com>
2021-08-08 22:24:06 -0400

    Debugging PDF download

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,12 +5,76 @@
 // WHEN I enter text into the email address field
 // THEN I receive a notification if I have entered an invalid email address
 
+import React, { useState } from 'react'
 
-import React from 'react'
+import { checkEmail } from '../utils/helper';
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+
+  const inputChangeHandler = (event) => {
+      const inputName = event.target.name;
+      const inputValue = event.target.value;
+      if (inputName === 'name') {
+          setName(inputValue);
+      } else if (inputName === 'email') {
+          setEmail(inputValue); 
+      } else if (inputName === 'message') {
+          setMessage(inputValue)
+      }
+  };
+  const formSubmit = (event) => {
+      event.preventDefault();
+      if (!name || !email || !message ) {
+          setError('Required field not completed')
+          return
+      }
+      if (!checkEmail(email)) {
+          setError('Please use a valid email');
+          return 
+      }
+      setName('');
+      setEmail('');
+      setMessage('');
+  }
     return (
-      <p>Contact</p>
+      <content>
+      <h1>Contact</h1>
+      <form className="form">
+                <input
+                value={name}
+                name="name"
+                onChange={inputChangeHandler}
+                type="text"
+                placeholder="Name"
+                />
+                <input
+                value={email}
+                name="email"
+                onChange={inputChangeHandler}
+                type="text"
+                placeholder="Email"
+                />
+                <textarea
+                value={message}
+                name="message"
+                onChange={inputChangeHandler}
+                type="text"
+                placeholder="Message"
+                />
+                <button type="button" onClick={formSubmit}>
+                Submit
+                </button>
+            </form>
+            {error && (
+                <div>
+                    <p>{error}</p>
+                </div>
+            )}
+      </content>
     );
 };
 
