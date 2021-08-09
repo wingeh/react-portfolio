e7c207c58b69c5f4e68a7b4da8fa3f44ea8808e6
Andrew <wing_andrew@hotmail.com>
2021-08-08 22:05:41 -0400

    Adding CV page with PDF download

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -2,11 +2,16 @@
 // THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
 
 
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
 
 function Cv() {
     return (
-      <p>Cv</p>
+      <div>
+      <h2>Curriculum Vitae</h2>
+      <Link to="./../downloads/pdf/cv.pdf" target="_blank" download><FaIcons.FaDownload />Download PDF</Link>
+      </div>
     );
 };
 
